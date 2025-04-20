1bb055162b655dbe4fed21932217fdf072885de5
Kavya-Garg003 <149413553+Kavya-Garg003@users.noreply.github.com>
2025-04-20 15:03:17 +0530

    Frontend

---
+++ scam_call/utils/audio_features.py
@@ -0,0 +1,8 @@
+import librosa
+import numpy as np
+
+def extract_audio_features(audio_path):
+    y, sr = librosa.load(audio_path, sr=None)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    mfcc_mean = np.mean(mfcc.T, axis=0)
+    return mfcc_mean
